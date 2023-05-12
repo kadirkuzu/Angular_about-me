34e9ae703921b12d230b281c5133fc8e01f11155
Kadir Kuzu <kadirkuzu.5656@gmail.com>
2023-05-12 23:10:52 +0300

    header canvas added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './components/pages/home-page/home-page.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
+import { SideBarComponent } from './components/layout/header/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/layout/header/side-bar/side-bar.component.ts
@@ -0,0 +1,24 @@
+import { AfterViewInit, Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-side-bar',
+  templateUrl: './side-bar.component.html',
+  styleUrls: ['./side-bar.component.scss']
+})
+export class SideBarComponent implements AfterViewInit {
+  @ViewChild('closeSideBarCanvas') closeSideBarCanvas!: ElementRef<HTMLElement>;
+  @ViewChild('sideBarCanvas') sideBarCanvas!: ElementRef;
+  @Output() closeEvent = new EventEmitter<any>();
+  
+  closeOffCanvas() {
+    let el: HTMLElement = this.closeSideBarCanvas.nativeElement;
+    el.click();
+  }
+
+  ngAfterViewInit(): void {
+    this.sideBarCanvas.nativeElement.addEventListener('hidden.bs.offcanvas', (event: any) => { 
+      this.closeEvent.emit(true)
+    })
+  }
+  
+}
